62673793c90d7ad649d5c9fa02b4c64d148a83fc
Alexandr Juncevich <a.juncevich@gmail.com>
2017-03-16 20:17:30 +0500

    Add some data in model

---
+++ app/model.ts
@@ -0,0 +1,7 @@
+var model = {
+    user : "Alex",
+    items: [{action: "Buy Flowers", done: false},
+    {action: "GetShoes", done: false},
+    {action: "Collect Tickets", done: true},
+    {action: "Call Joe", done: false}]
+}
