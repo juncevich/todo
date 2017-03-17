e409485cf096e507c64db443744d5e3d768ea6c8
Alexandr Juncevich <a.juncevich@gmail.com>
2017-03-17 08:31:43 +0500

    More code in model.ts

--- app/model.ts
+++ app/model.ts
@@ -1,7 +1,21 @@
-var model = {
-    user : "Alex",
-    items: [{action: "Buy Flowers", done: false},
-    {action: "GetShoes", done: false},
-    {action: "Collect Tickets", done: true},
-    {action: "Call Joe", done: false}]
+export class Model {
+    user;
+    items;
+
+    constructor() {
+        this.user = "Alex";
+        this.items = [new TodoItem("Buy Flowers", false),
+    new TodoItem("GetShoes", false),
+    new TodoItem("Collect Tickets", true),
+    new TodoItem("Call Joe", false)]
+    }
+}
+export class TodoItem {
+    action;
+    done;
+
+    constructor(action, done) {
+        this.action = action;
+        this.done = done;
+    }
 }
